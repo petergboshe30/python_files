c424d3b8e35eff5f72743ae5102843fbe66bfa80
petergboshe30 <petergboshe30@gmail.com>
2026-02-25 11:02:57 +0100

    Add files via upload

---
+++ dimensions.py
@@ -0,0 +1,15 @@
+# Define variables
+num1 = 25 
+num2 = 4
+
+#calculate and print resuits
+print(f"Number 1: {num1}")
+print(f"Number 2: {num2}/n")
+
+print(f"1.sum:{num1}+{num2}={num1+num2}")
+print(f"2.Difference:{num1}-{num2}={num1-num2}")
+print(f"3.product:{num1}*{num2}={num1*num2}")
+print(f"4.Divine:{num1}/{num2}={num1/num2}")
+print(f"5.flood divine:{num1}//{num2}={num1//num2}")
+print(f"6.Modulus:{num1}%{num2}={num1%num2}")
+print(f"7.Power:{num1}**{num2}={num1**num2}")
